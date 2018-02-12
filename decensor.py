8f7e46f5f005cb1b0cb547031ec20fecf1341b6e
deep pomf <deeppomf@gmail.com>
2018-02-12 12:25:10 -0500

    remove xrange for python 3 compat

--- decensor.py
+++ decensor.py
@@ -75,8 +75,8 @@ def get_mask(x_batch):
         raw = x_batch[i]
         raw = np.array((raw + 1) * 127.5, dtype=np.uint8)
         m = np.zeros((IMAGE_SIZE, IMAGE_SIZE, 1), dtype=np.uint8)
-        for x in xrange(IMAGE_SIZE):
-            for y in xrange(IMAGE_SIZE):
+        for x in range(IMAGE_SIZE):
+            for y in range(IMAGE_SIZE):
                 if np.array_equal(raw[x][y], [0, 255, 0]):
                     m[x, y] = 1
         mask.append(m)
